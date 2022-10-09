0d3584d3a41d6ecaa3668a1c91bb3281fc2f2712
BridgeQZH <zju_qzh@163.com>
2022-10-09 23:04:56 +0200

    transfer from YT colab

---
+++ main.py
@@ -0,0 +1,150 @@
+import os
+from PIL import Image, ImageDraw
+import cv2
+import numpy as np
+from IPython.display import HTML
+from base64 import b64encode
+
+import torch
+from torch import autocast
+from torch.nn import functional as F
+from diffusers import StableDiffusionPipeline, AutoencoderKL
+from diffusers import UNet2DConditionModel, PNDMScheduler, LMSDiscreteScheduler
+from diffusers.schedulers.scheduling_ddim import DDIMScheduler
+from transformers import CLIPTextModel, CLIPTokenizer
+from tqdm.auto import tqdm
+
+def get_text_embeds(prompt):
+    # Tokenize text and get embeddings
+    text_input = tokenizer(
+        prompt, padding='max_length', max_length=tokenizer.model_max_length,
+        truncation=True, return_tensors='pt')
+    with torch.no_grad():
+        text_embeddings = text_encoder(text_input.input_ids.to(device))[0]
+
+    # Do the same for unconditional embeddings
+    uncond_input = tokenizer(
+        [''] * len(prompt), padding='max_length',
+        max_length=tokenizer.model_max_length, return_tensors='pt')
+    with torch.no_grad():
+        uncond_embeddings = text_encoder(uncond_input.input_ids.to(device))[0]
+
+    # Cat for final embeddings
+    text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+    return text_embeddings
+
+def produce_latents(text_embeddings, height=512, width=512,
+                    num_inference_steps=50, guidance_scale=7.5, latents=None):
+  if latents is None:
+    latents = torch.randn((text_embeddings.shape[0] // 2, unet.in_channels, \
+                           height // 8, width // 8))
+  latents = latents.to(device)
+
+  scheduler.set_timesteps(num_inference_steps)
+  latents = latents * scheduler.sigmas[0]
+
+  with autocast('cuda'):
+    for i, t in tqdm(enumerate(scheduler.timesteps)):
+      # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+      latent_model_input = torch.cat([latents] * 2)
+      sigma = scheduler.sigmas[i]
+      latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
+
+      # predict the noise residual
+      with torch.no_grad():
+        noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings)['sample']
+
+      # perform guidance
+      noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+      noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+      # compute the previous noisy sample x_t -> x_t-1
+      latents = scheduler.step(noise_pred, i, latents)['prev_sample']
+  
+  return latents
+
+def decode_img_latents(latents):
+  latents = 1 / 0.18215 * latents
+
+  with torch.no_grad():
+    imgs = vae.decode(latents)
+
+  imgs = (imgs / 2 + 0.5).clamp(0, 1)
+  imgs = imgs.detach().cpu().permute(0, 2, 3, 1).numpy()
+  imgs = (imgs * 255).round().astype('uint8')
+  pil_images = [Image.fromarray(image) for image in imgs]
+  return pil_images
+
+def encode_img_latents(imgs):
+  if not isinstance(imgs, list):
+    imgs = [imgs]
+
+  img_arr = np.stack([np.array(img) for img in imgs], axis=0)
+  img_arr = img_arr / 255.0
+  img_arr = torch.from_numpy(img_arr).float().permute(0, 3, 1, 2)
+  img_arr = 2 * (img_arr - 0.5)
+
+  latent_dists = vae.encode(img_arr.to(device))
+  latent_samples = latent_dists.sample()
+  latent_samples *= 0.18215
+
+  return latent_samples
+
+def prompt_to_img(prompts, height=512, width=512, num_inference_steps=50,
+                  guidance_scale=7.5, latents=None):
+  if isinstance(prompts, str):
+    prompts = [prompts]
+
+  # Prompts -> text embeds
+  text_embeds = get_text_embeds(prompts)
+
+  # Text embeds -> img latents
+  latents = produce_latents(
+      text_embeds, height=height, width=width, latents=latents,
+      num_inference_steps=num_inference_steps, guidance_scale=guidance_scale)
+  
+  # Img latents -> imgs
+  imgs = decode_img_latents(latents)
+
+  return imgs
+
+
+
+
+device = 'cuda'
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    'CompVis/stable-diffusion-v1-4', revision='fp16',
+    torch_dtype=torch.float16, use_auth_token=True)
+pipe = pipe.to(device)
+
+
+# 1. Load the autoencoder model which will be used to decode the latents into image space. 
+vae = AutoencoderKL.from_pretrained(
+    'CompVis/stable-diffusion-v1-4', subfolder='vae', use_auth_token=True)
+vae = vae.to(device)
+
+# 2. Load the tokenizer and text encoder to tokenize and encode the text. 
+tokenizer = CLIPTokenizer.from_pretrained('openai/clip-vit-large-patch14')
+text_encoder = CLIPTextModel.from_pretrained('openai/clip-vit-large-patch14')
+text_encoder = text_encoder.to(device)
+
+# 3. The UNet model for generating the latents.
+unet = UNet2DConditionModel.from_pretrained(
+    'CompVis/stable-diffusion-v1-4', subfolder='unet', use_auth_token=True)
+unet = unet.to(device)
+
+# 4. Create a scheduler for inference
+scheduler = LMSDiscreteScheduler(
+    beta_start=0.00085, beta_end=0.012,
+    beta_schedule='scaled_linear', num_train_timesteps=1000)
+
+src_img_path = "/content/drive/MyDrive/real_speech_1.png"
+im = Image.open("/content/drive/MyDrive/real_speech_1.png").convert('RGB')
+a = np.asarray(im)
+img = Image.fromarray(a)
+img_latents = encode_img_latents([img])
+
+prompt = 'North Korean President Kim Jong Un is giving a speech.'
+new_img = prompt_to_img(prompt, num_inference_steps=30, latents=img_latents)[0]
+new_img
